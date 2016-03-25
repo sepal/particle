e84cd5a907c87c730288c400132d563dfd87b799
Sebastian <sep.gil@gmail.com>
2016-03-25 19:48:27 +0100

    Added logic to create basic requests and added tests for the particle api client.

--- particle/particle.go
+++ particle/particle.go
@@ -3,6 +3,7 @@ package particle
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
@@ -39,7 +40,8 @@ type Client struct {
 	Token string
 }
 
-// NewClient returns a new particle cloud api client.
+// NewClient returns a new particle cloud api client. If no httpClient was passed,
+// than a new one will be created.
 func NewClient(httpClient *http.Client, token string) *Client {
 	if httpClient == nil {
 		httpClient = http.DefaultClient
@@ -54,6 +56,11 @@ func NewClient(httpClient *http.Client, token string) *Client {
 		Token:     token}
 
 	return c
+}
+
+func (r *ErrorResponse) Error() string {
+	return fmt.Sprintf("%v %v: %d %v",
+		r.Response.Request.Method, r.Response.Request.URL, r.Response.StatusCode, r.Message)
 }
 
 // NewRequest generates a new API request with given request. The urlString should point
@@ -77,7 +84,7 @@ func (c *Client) NewRequest(method, urlString string, body interface{}) (*http.R
 		}
 	}
 
-	req, err := http.Request{method, url, buffer}
+	req, err := http.NewRequest(method, url.String(), buffer)
 
 	if err != nil {
 		return nil, err
@@ -91,24 +98,8 @@ func (c *Client) NewRequest(method, urlString string, body interface{}) (*http.R
 	return req, nil
 }
 
-func CheckResponse(r *http.Response) error {
-	if c := r.StatusCode; c >= 200 && c <= 299 {
-		return nil
-	}
-
-	errorResponse := &ErrorResponse{Response: r}
-	data, err = ioutil.ReadAll(r.Body)
-	if err == nil && len(data) > 0 {
-		err := json.Unmarshal(data, errorResponse)
-		if err != nil {
-			return err
-		}
-	}
-
-	return errorResponse
-}
-
-func (c *Client) Do(req *http.Request) (*http.Response, error) {
+// Do executes an http.Request and checks for any errors.
+func (c *Client) Do(req *http.Request, v interface{}) (*http.Response, error) {
 	resp, err := c.client.Do(req)
 
 	if err != nil {
@@ -121,7 +112,35 @@ func (c *Client) Do(req *http.Request) (*http.Response, error) {
 		}
 	}()
 
-	//response := Response{Response: r}
-	return nil, nil
+	err = CheckResponse(resp)
 
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(v)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, err
+}
+
+// Checks the API response of an http.Response object.
+func CheckResponse(r *http.Response) error {
+	if c := r.StatusCode; c >= 200 && c <= 299 {
+		return nil
+	}
+
+	errorResponse := &ErrorResponse{Response: r}
+	data, err := ioutil.ReadAll(r.Body)
+	if err == nil && len(data) > 0 {
+		err := json.Unmarshal(data, errorResponse)
+		if err != nil {
+			return err
+		}
+	}
+
+	return errorResponse
 }
---
+++ particle/particle_test.go
@@ -0,0 +1,91 @@
+package particle
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"reflect"
+	"testing"
+	"io/ioutil"
+)
+
+var (
+	mux    *http.ServeMux
+	client *Client
+	server *httptest.Server
+)
+
+func setup() {
+	mux = http.NewServeMux()
+	server = httptest.NewServer(mux)
+
+	client = NewClient(nil, "foo")
+	url, _ := url.Parse(server.URL)
+	client.BaseUrl = url
+}
+
+func teardown() {
+	server.Close()
+}
+
+func TestNewRequest(t *testing.T) {
+	token := "someRandomToken"
+	c := NewClient(nil, token)
+
+	type foo struct{
+		A string
+	}
+
+	inUrl, outUrl := "/foo", apiBaseUrl+"/foo"
+	inBody, outBody := &foo{"foo"}, `{"A":"foo"}` + "\n"
+
+	req, _ := c.NewRequest("GET", inUrl, inBody)
+
+	// Test if the inUrl expanded to the absolute url.
+	if req.URL.String() != outUrl {
+		t.Errorf("NewRequest with %v has URL = %v, expected %v", inUrl, req.URL, outUrl)
+	}
+
+	// Test if body was correctly JSON encoded.
+	body, _ := ioutil.ReadAll(req.Body)
+
+	if string(body) != outBody {
+		t.Errorf("NewRequest has body %v, expected %v", string(body), outBody);
+	}
+
+	reqToken := req.Header.Get("Authorization")
+	if reqToken != "Bearer: " + token {
+		t.Errorf("NewRequest had wrong token %v, should be %v", reqToken, token)
+	}
+}
+
+func TestDo(t *testing.T) {
+	setup()
+	defer teardown()
+
+	type foo struct {
+		A string
+	}
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if m := "GET"; m != r.Method {
+			t.Errorf("Request method = %v, expected %v", r.Method, m)
+		}
+		fmt.Fprint(w, `{"A": "a"}`)
+	})
+
+	req, _ := client.NewRequest("GET", "/", nil)
+	body := new(foo)
+
+	_, err := client.Do(req, body)
+	if err != nil {
+		t.Fatalf("Do(): %v", err)
+	}
+
+	expected := &foo{"a"}
+	if !reflect.DeepEqual(body, expected) {
+		t.Errorf("Response body = %v, expected %v", body, expected)
+	}
+
+}
